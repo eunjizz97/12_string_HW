f3aa438af80635962d6a5a343552d8af5ff5d2e0
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-22 22:27:04 +0900

    입력 받은 단어를 파일로 출력

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	FILE *fp = NULL;
+	char s1[100];
+	char s2[100]; 
+	char s3[100];
 	
-	char src[100] = "Programming course";
-	char dst[100];
+	fp = fopen("c:\\sample.txt", "w");
+	if(fp == NULL)
+		printf("파일 못 열음.\n");
+		
+	printf("input a word : ");
+	scanf("%s", s1);
+	printf("input a word : ");
+	scanf("%s", s2);
+	printf("input a word : ");
+	scanf("%s", s3);
 	
-	strcpy(dst, src);
+	fprintf(fp, "%s\n%s\n%s\n", s1, s2, s3);
 	
-	printf("%s (%d)\n", dst, strlen(dst));
+	fclose(fp);
 	
 	return 0;
 }
