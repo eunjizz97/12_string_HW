15e21fed1b13ef3b8e5fd611ecc982ea7c4222f1
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-23 10:55:41 +0900

    파일을 읽어서 내용 출력2

--- main.c
+++ main.c
@@ -7,13 +7,21 @@
 int main(int argc, char *argv[]) {
 	FILE *fp = NULL;
 	char c;
+	int i;
+	char str[100];
 		
 	fp = fopen("c:\\Code\\sample.txt", "r");
 	if(fp == NULL)
 		printf("파일 못 열음\n");
 		
-	while((c=fgetc(fp)) != EOF)
-		putchar(c);
+	for(i=0;i<3;i++){
+	
+		fgets(str, 100, fp);
+		printf("%s", str);
+		
+	}
+
+		
 	
 	fclose(fp);
 	
