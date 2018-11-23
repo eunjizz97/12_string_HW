cfbbe625da5ffd9595acbb46d4a8b445ee4b643e
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-23 11:02:58 +0900

    파일 복사 프로그램

--- main.c
+++ main.c
@@ -5,25 +5,34 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE *fp = NULL;
+	FILE *rfp = NULL;
+	FILE *wfp = NULL;
 	char c;
 	int i;
 	char str[100];
 		
-	fp = fopen("c:\\Code\\sample.txt", "r");
-	if(fp == NULL)
+	rfp = fopen("c:\\Code\\sample.txt", "r");
+	wfp = fopen("c:\\Code\\samplecopy.txt", "w");
+	
+	printf("original file name : sample.txt\n");
+	printf("copy file name : samplecopy.txt\n");
+	
+	if(rfp == NULL || wfp == NULL)
 		printf("파일 못 열음\n");
+	
+	else
+		printf("copy success!\n"); 
 		
 	for(i=0;i<3;i++){
 	
-		fgets(str, 100, fp);
-		printf("%s", str);
+		fgets(str, 100, rfp);
+		fprintf(wfp, "%s", str);
 		
 	}
 
-		
 	
-	fclose(fp);
+	fclose(rfp);
+	fclose(wfp);
 	
 	return 0;
 }
