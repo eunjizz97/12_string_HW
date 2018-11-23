03e2fc0419a743ed81f9efb56c9e8fc9d0188034
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-23 10:43:33 +0900

    입력받은 단어를 파일로 출력

--- main.c
+++ main.c
@@ -6,16 +6,22 @@
 
 int main(int argc, char *argv[]) {
 	FILE *fp = NULL;
-	char c;
-	
-	fp = fopen("c:\\sample.txt", "r");
-	if(fp = NULL)
+	char str1[100];
+	char str2[100];
+	char str3[100];
+		
+	fp = fopen("c:\\Code\\sample.txt", "w");
+	if(fp == NULL)
 		printf("파일 못 열음\n");
 		
-	while((c=fgetc(fp))!=EOF){		
-		putchar(c);
+	printf("input a word : ");
+	scanf("%s", str1);
+	printf("input a word : ");
+	scanf("%s", str2);
+	printf("input a word : ");
+	scanf("%s", str3);
 	
-	}
+	fprintf(fp, "%s\n%s\n%s\n", str1, str2, str3);
 	
 	fclose(fp);
 	
